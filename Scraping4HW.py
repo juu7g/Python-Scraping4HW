bdaeeee24e3339eb7be2c9550c6fb1959b246711
juu7g <juu7g.owl296@gmail.com>
2021-05-04 17:35:07 +0900

    First commit

---
+++ Scraping4HW.py
@@ -0,0 +1,202 @@
+"""
+ハローワークの求人情報を検索し結果をCSVファイルに出力する
+"""
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as Ec
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
+import datetime
+import csv, sys, os
+from tqdm import tqdm
+sys.path.append(os.path.dirname(sys.executable))
+import settings     # 選択項目を定義している
+
+url = "https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do?action=initDisp&screenId=GECA110010"
+url0 = "https://www.hellowork.mhlw.go.jp/kensaku"
+
+def get_job_dt(url):
+    """
+    求人情報詳細を開いて「仕事の内容」と「必要な経験など」を取得する
+
+    Args:
+        url:    求人情報詳細のURL
+
+    Return:
+        「仕事の内容」と「必要な経験など」の文字列
+    """
+    browser.get(url)
+    wait.until(Ec.url_contains(url))
+    _text = browser.find_element_by_id("ID_shigotoNy").text # 仕事の内容
+    try:    # 必要な経験などがあれば追加する
+        _text = _text + "\n【必要な経験など】\n" + browser.find_element_by_id("ID_hynaKikntShsi").text
+    except NoSuchElementException:
+        pass
+    # browser.close()
+    return _text
+
+try:
+    # ブラウザーを起動
+    options = Options()         # オプションインスタンス作成
+    options.headless = True     # ヘッドレスモード(ブラウザを見せない)
+    browser = webdriver.Firefox(executable_path=settings.executable_path, options=options)  # ブラウザインスタンス作成
+
+    # 待機
+    wait = WebDriverWait(browser, 15)  # Timeout 15秒（最大待ち時間）
+
+    # ハローワーク検索画面にアクセス
+    print("start browsing")
+    browser.get(url)
+    # 求人情報検索画面が表示されるまで待機
+    wait.until(Ec.title_contains("求人情報検索"))
+    print("got url, start selecting")
+
+    # 検索条件の設定 クリックするもの
+    # sttingsから値がTrueで設定されているもので辞書を作成
+    _kensaku = {k: v for k, v in settings._kensaku.items() if v and type(v) is bool}
+    for id in _kensaku:
+        browser.find_element_by_id(id).click()  # チェックボックスをオンにする
+
+    # 検索条件の設定 キー入力するもの
+    # sttingsから値が文字列で設定されているもので辞書を作成
+    _kensaku = {k: v for k, v in settings._kensaku.items() if v and type(v) is str}
+    for id, v in _kensaku.items():
+        browser.find_element_by_id(id).send_keys(v)     # 設定文字列をセットする
+
+    # 就業場所の設定
+    # 都道府県のselectタグを取得して、ID順にする。
+    sels = browser.find_elements_by_tag_name("select")          # selectタグは就業場所のみ
+    sels = sorted(sels, key=lambda x: x.get_attribute("id"))    # id属性でソート
+    # 市町村選択ボタンのタグを取得して、onclick属性順にする。selectタグと同期させるため。
+    # inputタグでvalueが選択を抽出、onclick属性でソート
+    buttons = browser.find_elements_by_css_selector("input.button")         # 他のボタンも含まれる
+    btns = [x for x in buttons if x.get_attribute("value") == "選択"]       # valueで選別可能
+    btns = sorted(btns, key=lambda x: x.get_attribute("onclick"))           # onclick属性にIDが含まれる
+    # 就業場所3か所の設定
+    for _sel, _btn, _tdk in zip(sels, btns, settings.tdks):
+        Select(_sel).select_by_visible_text(_tdk[0])
+        if len(_tdk) > 1:   # 市町村を選択する場合
+            _btn.click()    # 市町村選択画面表示
+            # 選択画面が表示されるまで待つ
+            wait.until(Ec.element_to_be_clickable((By.ID, "ID_rank1CodeMulti")))
+            element = browser.find_element_by_id("ID_rank1CodeMulti")
+            for _city in _tdk[1:]:
+                Select(element).select_by_visible_text(_city)
+            browser.find_element_by_id("ID_ok").click()    # OKをクリック
+            # 選択画面が閉じるまで待つ
+            wait.until(Ec.invisibility_of_element_located((By.ID, "ID_ok")))
+
+    # 職種
+    # inputタグでvalueが「職種を選択」を抽出、onclick属性でソート
+    btns = [x for x in buttons if x.get_attribute("value") == "職種を選択"]
+    btns = sorted(btns, key=lambda x: x.get_attribute("onclick"))
+    # 職種3か所の設定
+    for  _btn, _sksu in zip(btns, settings.sksus):
+        _btn.click()    # 職種選択画面表示
+        # 選択画面が表示されるまで待つ
+        wait.until(Ec.element_to_be_clickable((By.ID, "ID_rank1Code")))
+        _sel = browser.find_element_by_id("ID_rank1Code")          # selectタグは大分類のみ
+        Select(_sel).select_by_visible_text(_sksu[0])
+        element = browser.find_element_by_id("ID_rank2Codes")
+        for _city in _sksu[1:]:
+            Select(element).select_by_visible_text(_city)
+        browser.find_element_by_id("ID_ok").click()    # OKをクリック
+        # 選択画面が閉じるまで待つ
+        wait.until(Ec.invisibility_of_element_located((By.ID, "ID_ok")))
+
+    # 「詳細検索条件」をクリック
+    browser.find_element_by_id("ID_searchShosaiBtn").click()
+    # 選択画面が表示されるまで待つ
+    wait.until(Ec.visibility_of_element_located((By.ID, "ID_saveCondBtn")))
+
+    # 詳細検索条件の設定
+    # sttingsから値がTrueで設定されているもので辞書を作成
+    _detial = {k: v for k, v in settings._shosai_settei.items() if v and type(v) is bool}
+    for id in _detial:
+        browser.find_element_by_id(id).click()  # チェックボックスをオンにする
+        # 検索条件の設定 キー入力するもの
+    # sttingsから値が文字列で設定されているもので辞書を作成
+    _detial = {k: v for k, v in settings._shosai_settei.items() if v and type(v) is str}
+    for id, v in _detial.items():
+        browser.find_element_by_id(id).send_keys(v)     # 設定文字列をセットする
+
+
+    # 「OK」をクリック
+    browser.find_element_by_id("ID_saveCondBtn").click()
+    # 選択画面が閉じるまで待つ
+    wait.until(Ec.invisibility_of_element_located((By.ID, "ID_saveCondBtn")))
+    print("selected, start job search")
+
+    # 「検索」をクリック
+    browser.find_element_by_id("ID_searchBtn").click()
+    # 0件の時「ご希望の条件に合致する情報は見つかりませんでした」が出る
+    # 表示件数選択肢がクリックできるようになるまで待つ
+    wait.until(lambda x: 
+        Ec.element_to_be_clickable((By.ID, "ID_fwListNaviDispTop"))
+                or Ec.text_to_be_present_in_element((By.ID, "msg_area"), "ご希望の条件に合致する情報は見つかりませんでした。"))
+
+    try:    # 検索結果が0件の場合、ID_fwListNaviDispTopが見つからないので例外を出す
+        _sel = browser.find_element_by_id("ID_fwListNaviDispTop")
+    except NoSuchElementException:
+        raise Exception("ご希望の条件に合致する情報は見つかりませんでした")
+
+    Select(_sel).select_by_visible_text("50件")
+    # 表示件数選択肢がクリックできるようになるまで待つ
+    wait.until(Ec.element_to_be_clickable((By.ID, "ID_fwListNaviDispTop")))
+    print("got jobs, start analysis")
+
+    # 今見ているページをBeautifulSoupで解析
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+
+    # 「求人」のテーブルを検索
+    jobs = soup.find_all("table", class_="kyujin")
+
+    _table = []
+    _csv_header = []
+    # jobsの要素を処理し、プログレスバーを表示する。
+    for job in tqdm(jobs, unit="件", ncols=75):
+        # 1度だけCSVのヘッダーとなる情報を取得する
+        # 見出しとなるタグにはfbクラスが設定されている。
+        if not _csv_header:
+            _csv_header = [x.get_text(strip=True) for x in job.select("td.fb")]
+
+        # 職種の取得
+        head = [job.find("td", class_="m13").get_text()]
+        # 求人区分から公開範囲の取得
+        body1 = [x.get_text(strip=False) for x in job.select("tr.border_new td:not([class])")]
+        # 期限、特徴などの取得
+        body2 = [x.get_text(" ", strip=True) for x in job.select("tr:not([class]) > td:not([class])")]
+        # 詳細情報のURL取得 相対アドレスが返るので、外部アドレスに変える。
+        foot = [url0 + job.find("a", id="ID_dispDetailBtn")['href'][1:]]
+        # 詳細情報の仕事を取得し、置き換える。仕事の内容は、4番目
+        shigoto = [get_job_dt(foot[0])]
+        body1[3:4] = shigoto
+        # 1件分のデータを設定
+        _table.append(head + body1 + body2 + foot)
+
+except TimeoutException:
+    print("ブラウザタイムアウト プログラム要調査")
+except Exception as identifier:
+    print("プログラムのブラウザ操作エラー", identifier)
+finally:
+    # ブラウザーを終了
+    browser.quit()
+print("analyzed, start writing csv")
+
+# CSVに出力
+output_path = '{}.csv'.format(datetime.datetime.now().strftime("%m%d_%H%M_%S"))
+
+try:
+    with open(output_path, encoding="cp932", mode="w", newline="") as f:
+        _writer = csv.writer(f)
+        _writer.writerow(_csv_header)   # 見出しを出力
+        _writer.writerows(_table)       # データを出力
+except Exception as e:
+    print("CSVエラー", e)
+
+print("finished")
---
+++ settings.py
@@ -0,0 +1,145 @@
+﻿# ドライバ(geckodriver.exe)のパスを指定 exeファイル名を含めること
+executable_path = "C:\MyProgram\geckodriver.exe"
+
+# 選択項目
+
+# 都道府県 3つまで 市町村は5つまで
+tdks = [
+          ["東京都", "千代田区"]
+        , ["東京都", "港区"]
+        , ["埼玉県"]
+        ]
+
+# 職種 3つまで
+sksus = [["技術職（建設、開発、ＩＴ）、専門職", "ソフトウェア開発技術者、プログラマー", "その他の情報処理・通信技術者"]
+        , ["事務、管理職", "一般事務、事務補助"]
+        ]
+
+_kensaku = {	
+	  "ID_kSNoJo": ""						# text 求人番号
+	, "ID_kSNoGe": ""						# text 求人番号
+	, "ID_kjKbnRadioBtn1": False			# radio 一般求人
+	, "ID_ippanCKBox1": True				# checkbox フルタイム
+	, "ID_ippanCKBox2": False				# checkbox パート
+	, "ID_kjKbnRadioBtn2": False			# radio 新卒・既卒求人
+	, "ID_kjKbnRadioBtn3": False			# radio 季節求人
+	, "ID_kjKbnRadioBtn4": False			# radio 出稼ぎ求人
+	, "ID_kjKbnRadioBtn5": False			# radio 障害のある方のための求人
+	, "ID_sGSYACKBox1": False				# checkbox フルタイム
+	, "ID_sGSYACKBox2": False				# checkbox パート
+	, "ID_nenreiInput": "60"				# text 年齢
+	, "ID_nenreiCKBox1": False				# checkbox 不問のみ
+	, "ID_nenreiCKBox2": False				# checkbox 不問をのぞく
+	, "ID_sKGYBRUIJo1": ""					# text 職業分類
+	, "ID_sKGYBRUIGe1": ""					# text 職業分類
+	, "ID_sKGYBRUIJo2": ""					# text 職業分類
+	, "ID_sKGYBRUIGe2": ""					# text 職業分類
+	, "ID_sKGYBRUIJo3": ""					# text 職業分類
+	, "ID_sKGYBRUIGe3": ""					# text 職業分類
+	, "ID_koyoFltmCKBox1": False			# checkbox 正社員
+	, "ID_koyoFltmCKBox2": False			# checkbox 正社員以外
+	, "ID_koyoFltmCKBox3": False			# checkbox 有期雇用派遣労働者
+	, "ID_koyoFltmCKBox4": False			# checkbox 無期雇用派遣労働者
+	, "ID_koyoPartCKBox5": False			# checkbox パート労働者
+	, "ID_koyoPartCKBox6": False			# checkbox 有期雇用派遣パート
+	, "ID_koyoPartCKBox7": False			# checkbox 無期雇用派遣パート
+	, "ID_newArrivedCKBox1": False			# checkbox 新着（当日・前日）の求人情報から検索
+	, "ID_newArrivedCKBox2": True			# checkbox 新着（１週間以内）の求人情報から検索
+	, "ID_freeWordRadioBtn0": False			# radio ＯＲ検索
+	, "ID_freeWordRadioBtn1": False			# radio ＡＮＤ検索
+	, "ID_freeWordInput": ""				# text フリーワード
+	, "ID_nOTKNSKFreeWordInput": ""			# text をのぞく
+	, "ID_kJNoJo1": ""						# text 求人番号
+	, "ID_kJNoGe1": ""						# text 求人番号
+	, "ID_kJNoJo2": ""						# text 求人番号
+	, "ID_kJNoGe2": ""						# text 求人番号
+	, "ID_kJNoJo3": ""						# text 求人番号
+	, "ID_kJNoGe3": ""						# text 求人番号
+	, "ID_kJNoJo4": ""						# text 求人番号
+	, "ID_kJNoGe4": ""						# text 求人番号
+	, "ID_kJNoJo5": ""						# text 求人番号
+	, "ID_kJNoGe5": ""						# text 求人番号
+	, "ID_jGSHNoJo": ""						# text 事業所番号
+	, "ID_jGSHNoChuu": ""					# text 事業所番号
+	, "ID_jGSHNoGe": ""						# text 事業所番号
+	}
+
+_shosai_settei = {    
+      "ID_shoyoAriCKBox1":  False				#checkbox",,あり
+	, "ID_shgJnStaJiCmbBoxHH": ""				#select時
+	, "ID_shgJnStaFunCmbBoxMM": ""				#select分
+	, "ID_shgJnEndJiCmbBoxHH": ""				#select時
+	, "ID_shgJnEndFunCmbBoxMM": ""				#select分
+	, "ID_kiboShgJnCKBox1":  False				#checkbox",,交代制（シフト制）を含まない
+	, "ID_kiboShgJnCKBox2":  False				#checkbox",,裁量労働制を含まない
+	, "ID_kiboShgJnCKBox3":  False				#checkbox",,変形労働時間制を含まない
+	, "ID_jkgiRadioBtn0":  False				#radio",,指定しない
+	, "ID_jkgiRadioBtn1":  False				#radio",,あり
+	, "ID_jkgiRadioBtn2":  False				#radio",,なし
+	, "ID_thkin":  ""			        		#text",
+	, "ID_holidayCKBox1":  False				#checkbox",,月曜日
+	, "ID_holidayCKBox2":  False				#checkbox",,火曜日
+	, "ID_holidayCKBox3":  False				#checkbox",,水曜日
+	, "ID_holidayCKBox4":  False				#checkbox",,木曜日
+	, "ID_holidayCKBox5":  False				#checkbox",,金曜日
+	, "ID_holidayCKBox6":  True				#checkbox",,土曜日
+	, "ID_holidayCKBox7":  True				#checkbox",,日曜日
+	, "ID_holidayCKBox8":  True				#checkbox",,祝日
+	, "ID_shukFtskRadioBtn0":  False			#radio",,指定しない
+	, "ID_shukFtskRadioBtn1":  True			#radio",,毎週
+	, "ID_shukFtskRadioBtn2":  False			#radio",,その他
+	, "ID_hakenUkeoinCKBox1":  False			#checkbox",,派遣
+	, "ID_hakenUkeoinCKBox2":  False			#checkbox",,請負
+	, "ID_hakenUkeoinCKBox3":  False			#checkbox",,派遣・請負を含まない
+	, "ID_kanyuHknCKBox1":  False				#checkbox",,雇用保険
+	, "ID_kanyuHknCKBox2":  False				#checkbox",,労災保険
+	, "ID_kanyuHknCKBox3":  False				#checkbox",,健康保険
+	, "ID_kanyuHknCKBox4":  False				#checkbox",,厚生年金
+	, "ID_kanyuHknCKBox5":  False				#checkbox",,公務災害補償
+	, "ID_kanyuHknCKBox6":  False				#checkbox",,財形
+	, "ID_kanyuHknCKBox7":  False				#checkbox",,企業年金
+	, "ID_kanyuHknCKBox8":  False				#checkbox",,退職金制度
+	, "ID_kanyuHknCKBox9":  False				#checkbox",,退職金共済
+	, "ID_keiyakuKsnNoKnsiAriCKBox1":  False	#checkbox",,あり
+	, "ID_knsiAriCKBox1":  False				#checkbox",,原則更新
+	, "ID_knsiAriCKBox2":  False				#checkbox",,条件あり
+	, "ID_tnseiRadioBtn0":  False				#radio",,指定しない
+	, "ID_tnseiRadioBtn1":  False				#radio",,あり
+	, "ID_tnseiCmbBox":  ""						#select歳以上
+	, "ID_tnseiRadioBtn2":  False				#radio",,なし
+	, "ID_nyukyoKaCKBox1":  False				#checkbox",,単身用あり
+	, "ID_nyukyoKaCKBox2":  False				#checkbox",,世帯用あり
+	, "ID_riyoKanoNaTjsAriCKBox1":  False		#checkbox",,あり
+	, "ID_jgshMeiIn":  ""						#text",
+	, "ID_nozokuCKBox1":  False					#checkbox",,のぞく
+	, "ID_jginSuRadioBtn0":  False				#radio",,指定しない
+	, "ID_jginSuRadioBtn1":  False				#radio",,１０人以上
+	, "ID_jginSuRadioBtn2":  False				#radio",,１００人以上
+	, "ID_jginSuRadioBtn3":  False				#radio",,３００人以上
+	, "ID_jginSuRadioBtn4":  False				#radio",,１０００人以上
+	, "ID_kiboSuruSngBrui1In":  ""				#text",
+	, "ID_kiboSuruSngBrui2In":  ""				#text",
+	, "ID_kiboSuruSngBrui3In":  ""				#text",
+	, "ID_grkiFumonCKBox1":  False				#checkbox",,不問
+	, "ID_hynaKikntFumonCKBox1":  False			#checkbox",,不問
+	, "ID_hynaMenkyoSkkuFumonCKBox1":  False	#checkbox",,不問
+	, "ID_jdsMenkyoCKBox1":  False				#checkbox",,必須
+	, "ID_jdsMenkyoCKBox2":  False				#checkbox",,あれば尚可
+	, "ID_jdsMenkyoCKBox4":  False				#checkbox",,必須・あれば尚可をのぞく
+	, "ID_jdsMenkyoCKBox3":  False				#checkbox",,AT限定可
+	, "ID_menkyoSkku1In":  ""					#text",
+	, "ID_menkyoSkkuNo1CKBox1":  False			#checkbox",,のぞく
+	, "ID_menkyoSkku2In":  ""					#text",
+	, "ID_menkyoSkkuNo2CKBox1":  False			#checkbox",,のぞく
+	, "ID_menkyoSkku3In":  ""					#text",
+	, "ID_menkyoSkkuNo3CKBox1":  False			#checkbox",,のぞく
+	, "ID_sonotaCKBox1":  False					#checkbox",,書類選考なし
+	, "ID_sonotaCKBox2":  False					#checkbox",,正社員登用あり
+	, "ID_sonotaCKBox3":  False					#checkbox",,マイカー通勤可
+	, "ID_sonotaCKBox4":  True					#checkbox",,転勤の可能性なし
+	, "ID_sonotaCKBox5":  False					#checkbox",,在宅勤務
+	, "ID_sonotaCKBox6":  False					#checkbox",,駅近（最寄り駅から徒歩１０分以内）
+	, "ID_sonotaCKBox7":  False					#checkbox",,屋内の受動喫煙対策あり
+	, "ID_sonotaCKBox8":  False					#checkbox",,トライアル雇用併用求人
+    , "ID_sonotaCKBox9":  False					#checkbox",,UIJターン歓迎求人
+    }
